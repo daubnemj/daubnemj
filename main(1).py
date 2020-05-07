4684694c52913dc9c727815bcf072e92728faa21
Mike Daubney <59942387+daubnemj@users.noreply.github.com>
2020-05-07 17:37:57 -0400

    Add files via upload

---
+++ main(1).py
@@ -0,0 +1,15 @@
+#Declare variables
+miles=0.0
+gas=0.0
+mpg=0.0
+
+#Get Input
+miles= float(input('How many miles have you driven?'))
+gas= float(input('How many gallons of gas were used?'))
+
+#Do Processing
+mpg= miles/gas
+
+#Display Output
+print("Your car gets", mpg, "miles-per-gallon.")
+
